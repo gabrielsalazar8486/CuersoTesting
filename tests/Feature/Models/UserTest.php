a94ecf75825c55ccb09ebfd7d17741055eacfdd5
gabrielsalazar8486 <salazargabriel8486@gmail.com>
2021-10-08 11:08:45 -0400

    ejercicio video 13 curso testing laravel

---
+++ tests/Feature/Models/UserTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature\Models;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_user()
+    {
+        User::factory()->create([
+           'email' => 'i@admin.com'
+        ]);
+
+        $this->assertDatabaseHas('users', [
+            'email' => 'i@admin.com'
+        ]);
+
+        $this->assertDatabaseMissing('users', [
+            'email' => 'no@admin.com'
+        ]);
+    }
+}
