dc85d6e680f698506759c3fce2b0f4207f5d8e2e
gabrielsalazar8486 <salazargabriel8486@gmail.com>
2021-10-12 15:36:42 -0400

    video final

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,12 +7,27 @@
         <title>Laravel</title>
     </head>
     <body>
+    <form action="tags" method="POST">
+        @csrf
+        <input type="text" name="name"/>
+        <input type="submit" value="Guardar"/>
+    </form>
         <h4>Listado de Etiquetas</h4>
     <table>
         @forelse( $tags as $tag)
         <tr>
             <td>
                 {{ $tag->name }}
+            </td>
+            <td>
+                {{ $tag->slug }}
+            </td>
+            <td>
+                <form action="tags/{{$tag->id}}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <input type="submit" value="Eliminar">
+                </form>
             </td>
         </tr>
         @empty
